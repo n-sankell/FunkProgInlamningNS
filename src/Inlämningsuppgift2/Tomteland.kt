41693e467a547cfe8554f198cec86b198be490e7
Niklas Sankell <niklas_san@gmail.com>
2022-01-14 16:29:15 +0100

    solution to Tomteland!

--- src/Inlämningsuppgift2/Tomteland.kt
+++ src/Inlämningsuppgift2/Tomteland.kt
@@ -22,21 +22,77 @@ För att bli godkänd på uppgiften måste du använda rekursion.
 
 class Tomteland {
 
-    //TODO: skapa en datastruktur för att lagra tomtarna och deras relationer i
+    class SantaNode<String>(val name: String) {
+        private val underlings: MutableList<SantaNode<String>> = mutableListOf()
+        fun addUnderling(underling: SantaNode<String>) = underlings.add(underling)
+        fun getList(): List<SantaNode<String>> {
+            return underlings
+        }
+    }
+
+    private fun populateTree(): MutableList<SantaNode<String>> {
+        val tomten = SantaNode("Tomten")
+        val glader = SantaNode("Glader")
+        val butter = SantaNode("Butter")
+        val troger = SantaNode("Tröger")
+        val trotter = SantaNode("Trötter")
+        val blyger = SantaNode("Blyger")
+        val radjuret = SantaNode("Rådjuret")
+        val nyckelpigan = SantaNode("Nyckelpigan")
+        val haren = SantaNode("Haren")
+        val raven = SantaNode("Räven")
+        val skumtomten = SantaNode("Skumtomten")
+        val dammrattan = SantaNode("Dammråttan")
+        val grasuggan = SantaNode("Gråsuggan")
+        val myran = SantaNode("Myran")
+        val bladlusen = SantaNode("Bladlusen")
+        tomten.addUnderling(glader).and(tomten.addUnderling(butter))
+        glader.addUnderling(troger).and(glader.addUnderling(trotter).and(glader.addUnderling(blyger)))
+        butter.addUnderling(radjuret).and(butter.addUnderling(nyckelpigan).and(butter.addUnderling(haren)).and(butter.addUnderling(raven)))
+        trotter.addUnderling(skumtomten)
+        skumtomten.addUnderling(dammrattan)
+        raven.addUnderling(grasuggan).and(raven.addUnderling(myran))
+        myran.addUnderling(bladlusen)
+        return mutableListOf(tomten)
+    }
 
     // current namn är den tomte vars underlydande ni vill ta fram
     //res är listan som håller alla underlydande
     fun getUnderlings(currentName: String, res: MutableList<String>): List<String> {
-        //TODO, skriv denna metod, glöm inte att den ska vara rekursiv!
-        throw UnsupportedOperationException()
+        val tomten = populateTree()
+
+        fun addUnderlingsToList(nextLevel: List<SantaNode<String>>) {
+            for (i in nextLevel) {
+                if (!res.contains(i.name)) {
+                    res.add(i.name)
+                }
+                if (i.getList().isNotEmpty()) {
+                    i.getList().forEach { res.add(it.name) }
+                    addUnderlingsToList(i.getList())
+                }
+            }
+        }
+
+        fun searchTree(tomten: List<SantaNode<String>>) {
+            for (i in tomten) {
+                if (i.name == currentName) {
+                    if (i.getList().isNotEmpty()) {
+                        addUnderlingsToList(i.getList())
+                    }
+                } else {
+                    searchTree(i.getList())
+                }
+            }
+        }
+        searchTree(tomten)
+
+        return res
     }
 
     fun main() {
-
         //Exempel på anrop till den rekursiva funktionen getUnderlings,
         // här är tanken att hitta Gladers underlydande
         //listan fylls på successivt när vi rekurserar
-
         var list: MutableList<String> = mutableListOf()
         println(getUnderlings("Glader", list))
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import Inlämningsuppgift2.Tomteland;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        new Tomteland().main();
 
     }
 }
